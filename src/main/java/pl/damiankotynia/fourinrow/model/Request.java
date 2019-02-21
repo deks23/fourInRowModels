d811635989c142127c4b6d62fb0ad93d1e23d184
deks23 <deks232@gmail.com>
2019-02-22 00:23:07 +0100

    initial commit

---
+++ src/main/java/pl/damiankotynia/fourinrow/model/Request.java
@@ -0,0 +1,10 @@
+package pl.damiankotynia.fourinrow.model;
+
+import java.io.Serializable;
+
+public class Request implements Serializable {
+
+    public Request() {
+        System.out.println("zxc");
+    }
+}
